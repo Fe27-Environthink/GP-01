f0f4ec4e47a964846988c5321dd453821f5f7c5d
Hakim Firman <hakimastina@gmail.com>
2023-05-01 01:29:05 +0700

    add function for button 'artikel lainnya'
    menambahkan fungsi pada tombol artikel lainnya yang akan menambahakan 3 card artikel.

--- assets/js/article.js
+++ assets/js/article.js
@@ -6,22 +6,69 @@ fetch(endpoint)
         return response.json();
     })
     .then((data) => {
-    data.map((el) => {
-        article.innerHTML += `
+        // menampilkan data dengan index 0-2
+        for (let i = 0; i < 3; i++) {
+            article.innerHTML += `
+                    <div class="col-sm-4 mb-3 pt-4 col-artikel">
+                        <div class="card card-artikel">
+                            <img src=${data[i].images} class="card-img-top" alt="artikel">
+                            <div class="card-body">
+                                <h5 class="card-title">${data[i].titleArticle}</h5>
+                                <p class="card-text" style="color: #595959; text-align: justify;">${data[i].descArticle}</p>
+                                <p class="fw-bold" style="color: #6F7376;">
+                                    <span class="author">${data[i].author}</span> <span class="dot">.</span>
+                                    <span class="date">${data[i].date}</span>
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                
+                `
+            
+        }
+        let btnArtikel= document.getElementById("artikel-lainnya");
+
+        console.log(btnArtikel);
+        let index=3
+        // menambahkan 3 artikel setiap menekan tombol artikel lainnya
+       btnArtikel.addEventListener("click",()=>{
+           for (let i = index; i < index+3; i++) {
+            article.innerHTML += `
             <div class="col-sm-4 mb-3 pt-4 col-artikel">
                 <div class="card card-artikel">
-                    <img src=${el.images} class="card-img-top" alt="artikel">
+                    <img src=${data[i].images} class="card-img-top" alt="artikel">
                     <div class="card-body">
-                        <h5 class="card-title">${el.titleArticle}</h5>
-                        <p class="card-text" style="color: #595959; text-align: justify;">${el.descArticle}</p>
+                        <h5 class="card-title">${data[i].titleArticle}</h5>
+                        <p class="card-text" style="color: #595959; text-align: justify;">${data[i].descArticle}</p>
                         <p class="fw-bold" style="color: #6F7376;">
-                            <span class="author">${el.author}</span> <span class="dot">.</span>
-                            <span class="date">${el.date}</span>
+                            <span class="author">${data[i].author}</span> <span class="dot">.</span>
+                            <span class="date">${data[i].date}</span>
                         </p>
                     </div>
                 </div>
             </div>
         
         `
-    })
+           }
+           index=index+3
+        })
+    // data.map((el) => {
+    //     article.innerHTML += `
+    //         <div class="col-sm-4 mb-3 pt-4 col-artikel">
+    //             <div class="card card-artikel">
+    //                 <img src=${el.images} class="card-img-top" alt="artikel">
+    //                 <div class="card-body">
+    //                     <h5 class="card-title">${el.titleArticle}</h5>
+    //                     <p class="card-text" style="color: #595959; text-align: justify;">${el.descArticle}</p>
+    //                     <p class="fw-bold" style="color: #6F7376;">
+    //                         <span class="author">${el.author}</span> <span class="dot">.</span>
+    //                         <span class="date">${el.date}</span>
+    //                     </p>
+    //                 </div>
+    //             </div>
+    //         </div>
+        
+    //     `
+    // })
  })
+
