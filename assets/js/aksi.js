524b436796a6344aa3b05223ec8f83dd771c7799
Hakim Firman <hakimastina@gmail.com>
2023-05-03 00:39:05 +0700

    add function to button 'aksi lainnya'

--- assets/js/aksi.js
+++ assets/js/aksi.js
@@ -4,21 +4,67 @@ const fetchAksi = fetch("https://64506cd6e1f6f1bb22977ba9.mockapi.io/aksi")
 fetchAksi
 .then(response =>response.json())
 .then(data =>{
-    data.map((index) =>{
+    for (let i = 0; i < 3; i++) {
+        
         aksiList.innerHTML +=`
         <div class="col-md-4 col-sm-6 mb-4 ">
             <div class="card" >
-                <img src="https://i.ibb.co/hVRwTrJ/perbudakan-modern-di-laut-terus-berlanjut-polri-gagal-lindungi-abk-ikan-indonesia.jpg" class="card-img-top" alt="...">
+                <img src="${data[i].image}" class="card-img-top" alt="...">
                 <div class="card-body">
-                  <h5 class="card-title">${index.nama}</h5>
-                  <p class="card-text text-secondary"><icon class="fa fa-users text-secondary me-2 "> </icon>${index.jumlahDukungan} orang mendukung</p>
-                  <a href="detailAksi.html?id=${index.id}" id="btn-aksi"   class="btn btn-success bg-main d-block ">Pelajari Selengkapnya</a>
+                  <h5 class="card-title">${data[i].nama}</h5>
+                  <p class="card-text text-secondary"><icon class="fa fa-users text-secondary me-2 "> </icon>${data[i].jumlahDukungan} orang mendukung</p>
+                  <a href="detailAksi.html?id=${data[i].id}" id="btn-aksi"   class="btn btn-success bg-main d-block ">Pelajari Selengkapnya</a>
                 </div>
               </div>
               
         </div>
         `
+        
+    }
+    console.log(data);
+    const btnAksi = document.getElementById("btn-aksi-lainnya");
+    
+    console.log(btnAksi);
+    index=3
+       btnAksi.addEventListener("click",()=>{
+        for (let i = index; i < index+3 && i <data.length; i++) {
+            aksiList.innerHTML +=`
+        <div class="col-md-4 col-sm-6 mb-4 ">
+            <div class="card" >
+                <img src="${data[i].image}" class="card-img-top" alt="...">
+                <div class="card-body">
+                  <h5 class="card-title">${data[i].nama}</h5>
+                  <p class="card-text text-secondary"><icon class="fa fa-users text-secondary me-2 "> </icon>${data[i].jumlahDukungan} orang mendukung</p>
+                  <a href="detailAksi.html?id=${data[i].id}" id="btn-aksi"   class="btn btn-success bg-main d-block ">Pelajari Selengkapnya</a>
+                </div>
+              </div>
+              
+        </div>
+        `
+            
+        }
+        if (!index<data.length) {
+            btnAksi.style.visibility="hidden"
+        }
+        index=index+3
+          
+        
     })
+    // data.map((index) =>{
+    //     aksiList.innerHTML +=`
+    //     <div class="col-md-4 col-sm-6 mb-4 ">
+    //         <div class="card" >
+    //             <img src="${index.image}" class="card-img-top" alt="...">
+    //             <div class="card-body">
+    //               <h5 class="card-title">${index.nama}</h5>
+    //               <p class="card-text text-secondary"><icon class="fa fa-users text-secondary me-2 "> </icon>${index.jumlahDukungan} orang mendukung</p>
+    //               <a href="detailAksi.html?id=${index.id}" id="btn-aksi"   class="btn btn-success bg-main d-block ">Pelajari Selengkapnya</a>
+    //             </div>
+    //           </div>
+              
+    //     </div>
+    //     `
+    // })
 })
 
 
--- assets/js/detailAksi.js
+++ assets/js/detailAksi.js
@@ -5,9 +5,11 @@ const aksiId = urlParams.get('id');
 console.log(aksiId);
 const fetchDetailAksi = fetch(`https://64506cd6e1f6f1bb22977ba9.mockapi.io/aksi/${aksiId}`)
 
+
 fetchDetailAksi.then(response => response.json())
 .then(data=>{
     document.getElementById("title").textContent=data.nama;
+    document.getElementById("image").src=data.image
     document.getElementById("paragraf1").textContent=data.paragraf1;
     document.getElementById("paragraf2").textContent=data.paragraf2
     document.getElementById("paragraf-konklusi").textContent=data.paragrafKonklusi
@@ -17,3 +19,36 @@ fetchDetailAksi.then(response => response.json())
     let Persentase = (data.jumlahDukungan  / data.Target) * 100
     document.getElementById("bar-petisi").style.width=Persentase+"%"
 })
+
+
+// form petisi
+const formPetisi = document.getElementById("form-petisi");
+const modalPetisi = document.getElementById("modal-petisi")
+var modalInstance = new bootstrap.Modal(modalPetisi);
+const inputCheck = document.getElementById
+
+formPetisi.addEventListener("submit",(event)=>{
+    event.preventDefault();
+       
+    if (formValidation()) {
+        modalInstance.show();
+      }
+      
+    
+})
+
+// validasi check box di form perisi
+const checkPetisi = document.getElementById("flexCheckDefault")
+
+function formValidation() {
+    let valid=true
+    if (!checkPetisi.checked) {
+        valid = false
+        console.log("belum dicetak");
+    }
+    // else{
+    //     console.log("sukes  dicetak");
+    // }
+    return valid;
+    
+}
