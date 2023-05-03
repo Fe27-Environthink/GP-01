3eac38e3bc4959080c52e2e2fa769b60b1ddc1ca
Hakim Firman <hakimastina@gmail.com>
2023-05-03 08:43:13 +0700

    minor update aksi.js
    memeperbaiki beberapa pengkondisian yang kurang

--- assets/js/aksi.js
+++ assets/js/aksi.js
@@ -43,14 +43,26 @@ fetchAksi
     index=3
        btnAksi.addEventListener("click",()=>{
         for (let i = index; i < index+3 && i <data.length; i++) {
+            let link =`detailAksi.html?id=${data[i].id}`
+            let  classLink="btn-success bg-main" 
+            let desc =`${data[i].jumlahDukungan} orang mendukung`
+     
+             if (data[i].Target-data[i].jumlahDukungan==0) {
+                 classLink= "btn-secondary"
+                 desc = "Petisi Mencapai Kemenangan"
+             }
+             else if (data[i].jumlahDukungan==0) {
+                 desc = "belum ada dukungan"
+                 
+             }
             aksiList.innerHTML +=`
-        <div class="col-md-4 col-sm-6 mb-4 ">
+            <div class="col-md-4 col-sm-6 mb-4 ">
             <div class="card" >
                 <img src="${data[i].image}" class="card-img-top" alt="...">
                 <div class="card-body">
                   <h6 class="card-title">${data[i].nama}</h6>
-                  <p class="card-text text-secondary"><icon class="fa fa-users text-secondary me-2 "> </icon>${data[i].jumlahDukungan} orang mendukung</p>
-                  <a href="detailAksi.html?id=${data[i].id}" id="btn-aksi"   class="btn btn-success bg-main d-block ">Pelajari Selengkapnya</a>
+                  <p class="card-text text-secondary"><icon class="fa fa-users text-secondary me-2 "> </icon>${desc}</p>
+                  <a href="${link}" id="btn-aksi"    class="btn d-block ${classLink} ">Pelajari Selengkapnya</a>
                 </div>
               </div>
               
